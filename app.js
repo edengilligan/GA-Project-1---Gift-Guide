f7ab9edcc6ec73e169f8f23e24dba650e7af85c4
Eden Gilligan <edengilligan@Edens-MBP.modem>
2020-12-08 14:47:35 +1100

    api added

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log("app is running");
+
+$("#call-api-btn").on("click", () => {
+    console.log("Call api button was clicked!");
+    // console.log("ash");
+    // const movieName = $("#input-movie-box").val();
+    const movieData = $.get(
+      `https://api.quotable.io/random`,
+      (data) => {
+        console.log(data);
+        
+      }
+    );
+  });
